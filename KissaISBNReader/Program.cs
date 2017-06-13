59107413eac25c3bd1dabaf72085e919ad8a8fb4
marco_majoor@hotmail.com <8n8taZghKWz7>
2017-06-13 15:16:47 +0200

    Version 1.1 using bookfinder.com

--- KissaISBNReader/Program.cs
+++ KissaISBNReader/Program.cs
@@ -15,6 +15,9 @@ namespace KissaISBNReader {
       string filePath = @"C:\Users\marco\Downloads\ConLog.txt";
       string resultPath = @"C:\Users\marco\Downloads\Conreads.csv";
       Regex IsbnRegex = new Regex(@"(?<ISBN>\d+) (?<date>\d{4}-\d{2}-\d{2}) (?<time>\d{2}:\d{2}:\d{2})");
+      Dictionary<string, int> countdictionary = new Dictionary<string, int>();
+      Dictionary<string, string> titleDictionary = new Dictionary<string, string>();
+
 
       if (args.Length > 0) {
         // Filepath has been provided as argument
@@ -25,28 +28,53 @@ namespace KissaISBNReader {
         // another regex??
       }
 
-      using (StreamReader fileReader = new StreamReader(filePath)) {
-        using (StreamWriter resultWriter = new StreamWriter(resultPath, false)) {
+      using (StreamWriter resultWriter = new StreamWriter(resultPath, false)) {
+        using (StreamReader fileReader = new StreamReader(filePath)) {
           string contents = fileReader.ReadToEnd();
-          resultWriter.WriteLine("Title");
+          resultWriter.WriteLine("Title|Count");
           var matches = IsbnRegex.Matches(contents);
-          HtmlWeb web = new HtmlWeb();
+        
+          int lineCounter = 0;
 
           foreach (Match match in matches) {
             var isbn = match.Groups["ISBN"].Value;
-            HtmlDocument document = web.Load($"http://isbndb.com/search/all?query={isbn}");
-            var result = document.DocumentNode.SelectSingleNode("//div[@class='bookSnippetBasic']/h1[@itemprop='name']");
 
-            if (result != null) {
-              var booktitle = result.InnerText.Trim();
-              resultWriter.WriteLine(booktitle);
+            if (!titleDictionary.ContainsKey(isbn)) {
+              var bookTitle = $"{isbn} not found!";
+              try {
+
+                //HtmlDocument document = web.Load($"http://isbndb.com/search/all?query={isbn}");
+                //var result = document.DocumentNode.SelectSingleNode("//div[@class='bookSnippetBasic']/h1[@itemprop='name']");
+
+                //HtmlDocument document = web.Load($"https://isbnsearch.org/isbn/{isbn}");
+                //var result = document.DocumentNode.SelectSingleNode("//div[@class='bookinfo']/h1");
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument document = web.Load($"https://www.bookfinder.com/search/?isbn={isbn}&mode=advanced&st=sr&ac=qr");
+                var result = document.DocumentNode.SelectSingleNode("//div[@class='attributes']/div/a//span[@itemprop='name']");
+
+
+                if (result != null) {
+                  bookTitle = result.InnerText.Trim();
+                }
+              }
+              catch (Exception) {
+                Console.WriteLine($"Error ocurred on line {lineCounter}");
+              }
+              titleDictionary.Add(isbn, bookTitle);
+              countdictionary.Add(isbn, 1);
+              Console.WriteLine($"line {lineCounter}: {bookTitle}");
             }
             else {
-              resultWriter.WriteLine($"{isbn} not found!");
+              countdictionary[isbn] = countdictionary[isbn] + 1;
             }
           }
         }
+
+        foreach (var isbn in titleDictionary.Keys) {
+          resultWriter.WriteLine($"{titleDictionary[isbn]}|{countdictionary[isbn]}");
+        }
       }
+
       Console.WriteLine("Press any key to continue...");
       Console.ReadKey();
     }
